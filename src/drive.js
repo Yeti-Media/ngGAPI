9688e84fe64a6e5f87d617aed45ab469e561bfbc
Nicolas Santa <nicolas55ar@gmail.com>
2013-12-27 19:45:39 -0300

    better file and module organization

---
+++ src/drive.js
@@ -0,0 +1,75 @@
+
+  /**
+   * Drive API
+   */
+
+'use strict';
+
+angular.module('gapi')
+  .factory('Drive', function (GAPI) {
+    var Drive = new GAPI('drive', 'v2', {
+      files:          ['get', 'list', 'insert', 'update', 'delete', 'patch', {
+        children:     ['get', 'list', 'insert', 'delete'],
+        parents:      ['get', 'list', 'insert', 'delete'],        
+        permissions:  ['get', 'list', 'insert', 'update', 'delete', 'patch'],
+        revisions:    ['get', 'list', 'update', 'delete', 'patch'],
+        comments:     ['get', 'list', 'insert', 'update', 'delete', 'patch', {
+          replies:      ['get', 'list', 'insert', 'update', 'delete', 'patch']
+        }],
+        properties:   ['get', 'list', 'insert', 'update', 'delete', 'patch'],
+        realtime:     ['get']
+      }],
+      changes: ['get', 'list'],
+      apps: ['get', 'list']
+    });
+
+    Drive.copyFile = function (fileId, data, params) {
+      return Drive.post('files', fileId, 'copy', data, params);
+    };
+
+    Drive.touchFile   = function (fileId) {
+      return Drive.post('files', fileId, 'touch');  
+    };
+
+    Drive.trashFile   = function (fileId) {
+      return Drive.post('files', fileId, 'trash');
+    };
+
+    Drive.untrashFile = function (fileId) {
+      return Drive.post('files', fileId, 'untrash');      
+    };
+
+    Drive.watchFile   = function (fileId, data) {
+      return Drive.post('files', fileId, 'watch', data);
+    };
+
+    Drive.about = function (params) {
+      return Drive.get('about', params);
+    }
+
+    Drive.watchChanges = function (data) {
+      return Drive.post('changes', 'watch', data);
+    };
+
+    Drive.getPermissionIdForEmail = function (email) {
+      return Drive.get('permissionIds', email);
+    };
+
+    Drive.stopChannels = function (data) {
+      return Drive.post('channels', 'stop', data);
+    };
+
+    Drive.updateRealtime = function (fileId, params) {
+      return GAPI.request({
+        method: 'PUT', 
+        url:    Drive.url + ['files', fileId, 'realtime'].join('/'),
+        params: params
+      });
+    };
+
+
+    return Drive;
+  })
+
+
+
---
+++ src/youtube.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('gapi')
+  .factory('Youtube', function (GAPI) {
+    var Youtube = new GAPI('youtube', 'v3', {
+      activities:       ['list', 'insert'],
+      channels:         ['list', 'update'],
+      guideCategories:  ['list'],
+      liveBroadcasts:   ['list', 'insert', 'update', 'delete'],
+      liveStreams:      ['list', 'insert', 'update', 'delete'],
+      playlistItems:    ['list', 'insert', 'update', 'delete'],
+      playlists:        ['list', 'insert', 'update', 'delete'],
+      subscriptions:    ['list', 'insert', 'delete'],
+      thumbnails:       ['set'],
+      videoCategories:  ['list'],
+      videos:           ['list', 'insert', 'update', 'delete'],
+      watermarks:       ['set', 'unset']
+    });
+
+    // Some methods don't fit the pattern
+    // Define them explicitly here
+    Youtube.insertChannelBanners = function () {};
+
+    Youtube.bindLiveBroadcasts = function () {};
+    Youtube.controlLiveBroadcasts = function () {};
+    Youtube.transitionLiveBroadcasts = function () {};
+
+    Youtube.rateVideos = function (params) {
+      return Youtube.post('videos', 'rate', undefined, params);
+    };
+
+    Youtube.getVideoRating = function (params) {
+      return Youtube.get('videos', 'getRating', params);     
+    };
+
+    Youtube.search = function (params) {
+      return Youtube.get('search', params);
+    }
+
+    return Youtube;
+  });
+
+
+
